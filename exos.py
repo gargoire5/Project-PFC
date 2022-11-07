7ad30fcb019cfa09cf39608b546692934c6c46d5
gargoire5 <gportevin@gaming.tech>
2022-11-07 15:45:15 +0100

    change exo

---
+++ exos.py
@@ -0,0 +1,79 @@
+DEBUT
+# ECRIRE "Bonjour Monde"
+
+
+assertionUn = (( (365 * 3) / (24-(16-8)) ) * (rh) > r ) == (e * s < r)
+
+(( (365 * 3) / (24-(16-8)) ) * (rh) > r ) True or False ?
+(16-8= 8)
+(24-8 = 16)
+(365 * 3 ) = 1095
+(1095/16) = 68.4375
+(68.4375 * 230) = 15740.625
+15740.625 > 12000 True
+
+(e * s < r) True or False ?
+(10*1250) = 12500
+12500 < 12000 False 
+
+# assertionUn = assertUnPtUn = assertUnPtDeux # False
+r = 12000
+s = 1250
+e = 10
+rh = 230
+
+
+assertionDeux = ((365 * 3) / (4 - (12 - 8)) * (rh) > r) == (e * s < r)
+
+(( (365 * 3) / (4-(12-8)) ) * (rh) > r ) True or False ?
+(12-8= 4)
+(4-4 = 0)
+(365 * 3 ) = 1095
+(1095/0) = error false 
+(68.4375 * 230) = 15740.625
+15740.625 > 12000 False
+
+(e * s < r) True or False ?
+(10*1250) = 12500
+12500 < 12000 False 
+
+
+# assertionUn = assertUnPtUn = assertUnPtDeux # True
+
+
+def retournerSixPlusTrois():
+    return 6 + 3
+def retournerSixPlusX(){
+    return 6 + 3
+}
+print("Qui vole un " + retournerSixPlusTrois() "vole un boeuf.")
+
+
+-add (x,y)
+def add(x,y):
+    return x + y
+
+-sub (x,y)
+def sub(x,y):
+    return x - y
+-multiply (x,y)
+def multiply(x,y):
+    return x * y
+-divide (x,y)
+def divide(x,y):
+    if y == 0:
+        return "error"
+    else :
+        return x / y
+
+
+-modulo (x,y)
+-Calcule de salaire que je peux gagner par seconde:
+Salaire horaire;
+Heure par jour ouvrable;
+Nb jour ouvrabe;
+-Calcule salaire net:
+Salaire Brut;
+Coeff;
+
+FIN
